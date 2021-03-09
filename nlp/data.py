93716791cb47b1af5988f8c5fa34101ccb55b8db
Daniel Korat <daniel.korat@intel.com>
2021-03-09 15:11:05 +0200

    organize data.py

--- nlp/data.py
+++ nlp/data.py
@@ -16,9 +16,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 CACHE_DIR = Path(dirname(realpath(__file__))) / 'data'
-DATA_DIR = CACHE_DIR / "conll_data"
 VECTORS_DIR = CACHE_DIR / ".vector_cache"
-WORD_COUNT_DATA = CACHE_DIR / 'word_count.pickle'
 
 VECTORS = CharNGram
 
@@ -34,7 +32,7 @@ class HuggingfaceDataModule(pl.LightningDataModule):
 
     @staticmethod
     @abstractmethod
-    def download_and_preprocess(ds):
+    def download_and_preprocess():
         raise NotImplementedError()
 
     def setup(self, stage):
@@ -58,18 +56,24 @@ class HuggingfaceDataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return self.split_dataloader(self.test_dataset)
 
+
 class WordCountDataModule(HuggingfaceDataModule):
+    ds_name = "conll2003"
+    ds_cache = CACHE_DIR / 'word_count.pickle'
+    data_dir = CACHE_DIR / 'conll_data'
+
     def __init__(self, config):
         super().__init__(config)
 
     @staticmethod
-    def download_and_preprocess(ds_name="conll2003"):
-        if isfile(WORD_COUNT_DATA):
-            with open(WORD_COUNT_DATA, 'rb') as ds_pickle:
+    def download_and_preprocess():
+        if isfile(WordCountDataModule.ds_cache):
+            with open(WordCountDataModule.ds_cache, 'rb') as ds_pickle:
                 word_count_ds = pickle.load(ds_pickle)
 
         else:
-            conll_dataset = load_dataset(ds_name)
+            conll_dataset = load_dataset(path=WordCountDataModule.ds_name,
+                data_dir=WordCountDataModule.data_dir)
             embedder = VECTORS(cache=VECTORS_DIR)
 
             counts = defaultdict(int)
@@ -81,29 +85,45 @@ class WordCountDataModule(HuggingfaceDataModule):
             for t, c in counts.items():
                 word_count_ds.append((embedder[t].float(), torch.tensor([log(c)], dtype=torch.float)))
 
-            with open(WORD_COUNT_DATA, 'wb') as ds_pickle:
+            with open(WordCountDataModule.ds_cache, 'wb') as ds_pickle:
                 pickle.dump(word_count_ds, ds_pickle)
         return word_count_ds
 
-if __name__=="__main__":
-    DS_NAME = "conll2003"
 
-    counts = defaultdict(int)
-    ds = load_dataset(DS_NAME)
-    for split in 'train', 'test', 'validation':
-        for example in tqdm(ds[split]):
-            for token in example['tokens']:
-                counts[token.lower()] += 1
+class WikiDataModule(HuggingfaceDataModule):
+    ds_name = "wikicorpus"
+    ds_cache = CACHE_DIR / 'wiki.pickle'
+    data_dir = CACHE_DIR / 'wiki_data'
 
-    log_counts = defaultdict(int)
-    for w, count in counts.items():
-        log_counts[w] = log(count)
-    sorted_counts = sorted(log_counts.values(), reverse=True)
-    df = pd.DataFrame(data=sorted_counts)
-    df.index = log(df.index)
+    def __init__(self, config):
+        super().__init__(config)
 
-    df.plot()
-    plt.xlabel("sorted items in log scale")
-    plt.ylabel("frequency in log scale")
-    plt.legend([DS_NAME])
-    plt.show()
+    @staticmethod
+    def download_and_preprocess():
+        conll_dataset = load_dataset(path=WikiDataModule.ds_name, name='raw_en',
+            data_dir=WikiDataModule.data_dir)
+
+if __name__== "__main__":
+    ds = WikiDataModule.download_and_preprocess()
+
+    # DS_NAME = "conll2003"
+
+    # counts = defaultdict(int)
+    # ds = load_dataset(DS_NAME)
+    # for split in 'train', 'test', 'validation':
+    #     for example in tqdm(ds[split]):
+    #         for token in example['tokens']:
+    #             counts[token.lower()] += 1
+
+    # log_counts = defaultdict(int)
+    # for w, count in counts.items():
+    #     log_counts[w] = log(count)
+    # sorted_counts = sorted(log_counts.values(), reverse=True)
+    # df = pd.DataFrame(data=sorted_counts)
+    # df.index = log(df.index)
+
+    # df.plot()
+    # plt.xlabel("sorted items in log scale")
+    # plt.ylabel("frequency in log scale")
+    # plt.legend([DS_NAME])
+    # plt.show()
